032a5dc328ab2de4d2853998b09db81e546f4133
Jesstern Rays <jsstrn@users.noreply.github.com>
2026-03-13 17:02:40 +0800

    Add backend state file

--- terraform.tf
+++ terraform.tf
@@ -7,4 +7,10 @@ terraform {
       version = "~> 6.36.0"
     }
   }
+
+  backend "s3" {
+    bucket = "sctp-ce12-tfstate-bucket"
+    key = "jsrn/tf-learn-terraform/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
 }
